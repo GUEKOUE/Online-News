61ef8c34f26f7bb571658f2e1176409e461c93c0
GUEKOUE <compassiondemenou@yahoo.com>
2022-08-16 18:18:26 +0100

    edited welcome.blade.php file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
         <!-- Styles -->
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="/css/app.css" rel="stylesheet">
 
         <style>
             body {

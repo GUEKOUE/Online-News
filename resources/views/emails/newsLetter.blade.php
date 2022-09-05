db9849a9db0a09032b7154f034e7233b9bf7af30
GUEKOUE <compassiondemenou@yahoo.com>
2022-09-05 09:01:33 +0100

    added link to newsletter.blade

--- resources/views/emails/newsLetter.blade.php
+++ resources/views/emails/newsLetter.blade.php
@@ -6,7 +6,7 @@
 <body>
  
  <h2>Congratulation {{ $user->name }}, you have successfully subscribed to our Newsletter !</h2>
- <p>We will get you in touch readily. Find the best updated news from this site <a href=""></a></p>
+ <p>We will get you in touch readily. Find the best updated news from this site <a href="https://gdac-news.herokuapp.com"></a></p>
  
  Thanks.
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,9 +2,10 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>News Application</title>
+        <title>GDAC | News</title>
 
         <!-- Fonts -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet">
